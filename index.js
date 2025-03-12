b91aabd926cb65596e2339406a0dbc4fb18ffa95
natashanyokabi <natashawairimu27@gmail.com>
2025-03-12 21:34:54 +0300

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    const hq = 42;
+    return Math.abs(street - hq);
+}
+
+function distanceFromHqInFeet(street){
+    const block = distanceFromHqInBlocks(street);
+    return block * 264;
+}
+
+function distanceTravelledInFeet(start, end){
+    const distanceTravelledInFeet = Math.abs(end - start)
+    return distanceTravelledInFeet * 264
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination) 
+    if(distance<=400){
+        return 0
+    }else if(distance > 400 && distance < 2000){
+        return (distance - 400)* 0.02
+    }else if (distance >2000 && distance<= 2500){
+        return 25
+    }else{
+        return 'cannot travel that far'
+    }
+}
